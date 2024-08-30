3b2897b95e7c833fba83527d3d6be76ec3dfd7aa
Showvaggo <sr8950601@gmail.com>
2024-08-29 21:42:49 -0400

    hw

---
+++ src/practice_01_java_method/Showndorjo1.java
@@ -0,0 +1,24 @@
+package practice_01_java_method;
+
+import java.security.PublicKey;
+
+public class Showndorjo1 {
+	public String reletion="My doughter";
+	public int age=7;
+	public void showndorjo1reletion
+	() {
+		System.out.println("Me and Showndorjo1 relation is :" + reletion);
+		
+	
+	
+
+	}		
+		
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
